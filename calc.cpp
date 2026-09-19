0628c8228b52df708aa4095ca0b6a3d37947a9a5
zjhsyqh <zjh98508@outlook.com>
2026-09-19 15:31:39 +0800

    feat: 添加计算功能

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void calc(inta,int b)
+{
+    cout<<"本次输入的两个值之和为: "<<a+b<<endl;
+}
+int main()
+{
+   int  x,y;
+   cin>>x>>y;
+   calc(x,y);
+    return 0;
+}
